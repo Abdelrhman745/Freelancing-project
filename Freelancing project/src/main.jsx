3334dd61ee683c73aac8161739245569bc382624
Abdallah-Elsaid19 <abdoelsaid863@gmail.com>
2025-10-06 23:10:30 +0300

    import fontawesome

--- Freelancing project/src/main.jsx
+++ Freelancing project/src/main.jsx
@@ -7,9 +7,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import "@fontsource/tajawal";
 import "./index.css"
-
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import Layout from "./Layout/Layout.jsx";
 import Home from "./Component/HomePage/Home.jsx";
+
+
 
 const Routs = createBrowserRouter([
   {
