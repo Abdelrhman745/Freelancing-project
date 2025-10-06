c32e28d86b43af3287ff89541abfe4f04e6aceb3
Abdallah-Elsaid19 <abdoelsaid863@gmail.com>
2025-10-06 23:10:50 +0300

    import poprular and new collection in home

--- Freelancing project/src/Component/HomePage/Home.jsx
+++ Freelancing project/src/Component/HomePage/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Hero from "./Hero/Hero";
+import NewCollection from "./NewCollection/NewCollection";
+import Popular from "./Popular/Popular";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Popular />
+      <NewCollection />
     </>
   );
 }
